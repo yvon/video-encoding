85972958944002f0efaf2b35013dac4f9c100c16
Yvon <yvon.cognard@gmail.com>
2009-04-22 15:39:30 +0200

    Size in MB

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,5 +1,12 @@
 module Merb
   module GlobalHelpers
-    # helpers defined here available to all views.  
+    # helpers defined here available to all views.
+    
+    def number_to_human_size(number)
+      return nil if number.nil?
+      
+      ((number.to_f / 1024**2 * 100).round.to_f / 100).to_s + " MB"
+    end
+    
   end
 end
